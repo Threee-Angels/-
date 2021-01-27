f088fd634576ca88867f223e1e73048f0fa19623
Threee-Angels <77892785+Threee-Angels@users.noreply.github.com>
2021-01-27 16:58:52 +0300

    Create example1.py

---
+++ task8/example1.py
@@ -0,0 +1,14 @@
+# https://kpolyakov.spb.ru/school/ege/gen.php?action=viewVar&answers=on&varId=11
+l = ('А', 'Б', 'А', 'К')
+k = 0
+ans = set()
+for el1 in l:
+    for el2 in l:
+        for el3 in l:
+            for el4 in l:
+                s = el1 + el2 + el3 + el4
+                if s.count('А') == 2 and s.count('Б') == 1 and s.count('К') == 1:
+                    if s.count('АА') == 0:
+                        ans.add(s)
+                        k+=1
+print(len(ans))
