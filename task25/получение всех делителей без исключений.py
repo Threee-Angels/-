2c4e8888263fab263258db97d06a883ebec2bffb
Threee-Angels <77892785+Threee-Angels@users.noreply.github.com>
2021-04-21 17:14:48 +0300

    Update получение всех делителей без исключений.py

--- task25/получение всех делителей без исключений.py
+++ task25/получение всех делителей без исключений.py
@@ -1,13 +1,17 @@
 #https://inf-ege.sdamgia.ru/problem?id=27853
+from math import sqrt
 def get_div(n):
-    d = [1]
-    for i in range(2, n // 2+1):
+    d =[]
+    for i in range(1, int(sqrt(n))+1):
         if n % i ==0:
-            d.append(i)
-    d.append(n)
+            if i % 2 ==0:
+                d.append(i)
+            if n // i % 2 == 0:
+                d.append(n//i)
+    d.sort()
     return d
 
-for i in range(312614, 312651+1):
+for i in range(125256,125330+1):
     r = get_div(i)
     if len(r) == 6:
         print(r)
